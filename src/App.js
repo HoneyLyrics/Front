ef6b3ea0e57d71408953c09c5f26add3050da460
unknown <wkdus7h@naver.com>
2021-06-03 19:15:20 +0900

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import Home from './page/Home';
+import MusicDetail from './page/MusicDetail';
+import MusicList from './page/MusicList';
+
 const App = () => {
-  return <div>Honey Lyrics</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/musiclist/:moodid" component={MusicList} />
+        <Route path="/musicdetail/:songid" component={MusicDetail} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
