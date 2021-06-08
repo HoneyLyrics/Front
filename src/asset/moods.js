5dc5b4efc3dcabb5e55f6964d7b5b4e9c8e749f9
unknown <wkdus7h@naver.com>
2021-06-08 20:24:08 +0900

    Create moods

---
+++ src/asset/moods.js
@@ -0,0 +1,47 @@
+export const moods = [
+  {
+    moodName_kr: '유쾌한',
+    moodName: 'cheerful',
+    color: '',
+  },
+  {
+    moodName_kr: '음침한',
+    moodName: 'gloomy',
+    color: '',
+  },
+  {
+    moodName_kr: '익살스러운',
+    moodName: 'humorous',
+    color: '',
+  },
+  {
+    moodName_kr: '우울한',
+    moodName: 'melancholy',
+    color: '',
+  },
+  {
+    moodName_kr: '로맨틱한',
+    moodName: 'romantic',
+    color: '',
+  },
+  {
+    moodName_kr: '신비한',
+    moodName: 'mysterious',
+    color: '',
+  },
+  {
+    moodName_kr: '차분한',
+    moodName: 'calm',
+    color: '',
+  },
+  {
+    moodName_kr: '희망적인',
+    moodName: 'hopeful',
+    color: '',
+  },
+  {
+    moodName_kr: '외로운',
+    moodName: 'lonely',
+    color: '',
+  },
+];
