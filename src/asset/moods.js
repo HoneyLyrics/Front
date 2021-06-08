cbc8d472b26983e0e1d2b505db1d4fde1036e189
unknown <wkdus7h@naver.com>
2021-06-08 22:08:45 +0900

    Create moods data

--- src/asset/moods.js
+++ src/asset/moods.js
@@ -1,47 +1,58 @@
-export const moods = [
+const moods = [
   {
+    id: '1',
     moodName_kr: '유쾌한',
     moodName: 'cheerful',
-    color: '',
+    color: 'aqua',
   },
   {
+    id: '2',
     moodName_kr: '음침한',
     moodName: 'gloomy',
-    color: '',
+    color: 'lightslategray',
   },
   {
+    id: '3',
     moodName_kr: '익살스러운',
     moodName: 'humorous',
-    color: '',
+    color: 'deepskyblue',
   },
   {
+    id: '4',
     moodName_kr: '우울한',
     moodName: 'melancholy',
-    color: '',
+    color: 'lightpink',
   },
   {
+    id: '5',
     moodName_kr: '로맨틱한',
     moodName: 'romantic',
-    color: '',
+    color: 'salmon',
   },
   {
+    id: '6',
     moodName_kr: '신비한',
     moodName: 'mysterious',
-    color: '',
+    color: 'radial-gradient(black, transparent)',
   },
   {
+    id: '7',
     moodName_kr: '차분한',
     moodName: 'calm',
-    color: '',
+    color: 'aquamarine',
   },
   {
+    id: '8',
     moodName_kr: '희망적인',
     moodName: 'hopeful',
-    color: '',
+    color: 'lightsalmon',
   },
   {
+    id: '9',
     moodName_kr: '외로운',
     moodName: 'lonely',
-    color: '',
+    color: 'lemonchiffon',
   },
 ];
+
+export default moods;
