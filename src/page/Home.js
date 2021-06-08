a1ea146ccff39f040e7e5140f906127d0155bcb5
unknown <wkdus7h@naver.com>
2021-06-08 22:11:11 +0900

    Apply style to home component

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
+import Searchbar from '../components/Searchbar';
+import MoodList from '../components/MoodList';
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Header />
+      <Searchbar />
+      <MoodList />
+    </div>
+  );
 };
 
 export default Home;
