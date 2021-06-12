b20e696a64cbae9110cc99b6c59768552d2b0750
unknown <wkdus7h@naver.com>
2021-06-12 21:27:19 +0900

    Add songs bulk data

---
+++ src/asset/songs.js
@@ -0,0 +1,446 @@
+const songs = [
+  {
+    songId: '33433769',
+    title: 'Save Your Tears (Remix)',
+    imgUrl:
+      'https://image.bugsm.co.kr/album/images/original/153970/15397087.jpg?version=undefined',
+    artist: 'The Weeknd, Ariana Grande',
+    lyrics: `Ooh Ooh
+    Na na yeah
+    I saw you dancing
+    in a crowded room
+    You look so happy
+    when I'm not with you
+    But then you saw me
+    caught you by surprise
+    A single teardrop falling
+    from your eye
+    I don't know why I run away
+    Oh oh oh oh oh
+    I'll make you cry when I run away
+    Oh oh oh oh oh
+    Take me back 'cause I wanna stay
+    Save your tears for another
+    Save your tears for another day
+    Oh oh oh oh oh
+    Save your tears for another day
+    Met you once under a Pisces moon
+    I kept my distance
+    'cause I know that you
+    Don't like when I'm
+    with nobody else
+    I couldn't help it
+    I put you through hell
+    I don't know why I run away
+    Oh oh oh oh oh
+    Oh boy I'll make you cry
+    when I run away
+    Oh oh oh oh oh
+    Boy take me back
+    'cause I wanna stay
+    Save your tears for another
+    I realize that it's much too late
+    And you deserve someone better
+    Save your tears for another day
+    Oh oh oh oh oh
+    Save your tears for another day
+    Oh oh oh oh oh
+    I don't know why I run away
+    Bum bum bum bum bum
+    I'll make you cry
+    when I run away Save
+    Save your tears
+    for another day Ooh
+    ooh girl
+    Oh oh oh oh oh okay
+    I said save your tears
+    for another day
+    Oh oh oh oh oh
+    Save your tears
+    for another day Ah
+    Save your tears
+    for another day Ah`,
+  },
+  {
+    songId: '32962945',
+    title: 'Levitating (feat. DaBaby)',
+    imgUrl:
+      'https://image.bugsm.co.kr/album/images/original/9969/996989.jpg?version=undefined',
+    artist: 'Dua Lipa',
+    lyrics: `Billboard Baby
+    Dua Lipa make 'em dance
+    when it come on
+    Everybody lookin'
+    for a dancefloor to run on
+    If you wanna run away with me
+    I know a galaxy
+    And I can take you for a ride
+    I had a premonition
+    that we fell into a rhythm
+    Where the music don't stop
+    for life
+    Glitter in the sky
+    glitter in my eyes
+    Shining just the way I like
+    If you're feeling like you
+    need a little bit of company
+    You met me at the perfect time
+    You want me I want you baby
+    My sugarboo I'm levitating
+    The Milky Way we're renegading
+    Yeah yeah yеah yeah yeah
+    I got you moonlight
+    you're my starlight
+    I need you all night
+    come on dance with me
+    I'm levitating
+    You moonlight
+    you're my starlight
+    You're the moonlight
+    I need you all night
+    come on dance with me
+    I'm levitating
+    I'm one of the greatest
+    ain't no debatin' on it
+    Let's go
+    I'm still levitating
+    I'm heavily medicated
+    Ironic I gave 'em love
+    and they end up
+    hatin' on me Go
+    She told me love me
+    and she been waitin'
+    Been fightin' hard for your love
+    and I'm runnin' thin
+    on my patience
+    Needed someone to hug
+    even took it back to the basics
+    You see what you got me
+    out here doin' Yeah
+    Mighta threw me off
+    but can't nobody
+    stop the movement
+    Uh-uh let's go
+    Left foot right foot
+    levitatin' C'mon
+    Pop stars Go
+    Dua Lipa with DaBaby
+    I had to lace my shoes for all
+    the blessings I was chasin' Go
+    If I ever slip
+    I'll fall into a better situation
+    So catch up
+    go put some cheese on it
+    Get out and get
+    your bread up Yeah yeah
+    They always leave when you fall
+    but you run together Hey
+    Weight of the world
+    on my shoulders
+    I kept my head up
+    Now baby stand up
+    'cause girl you
+    You want me Hey
+    I want you baby Hey
+    My sugarboo I'm levitating
+    The Milky Way we're renegading
+    Yeah yeah yeah yeah yeah
+    I got you moonlight
+    you're my starlight
+    I need you all night
+    come on dance with me
+    I'm levitating
+    You moonlight
+    you're my starlight
+    You're the moonlight
+    I need you all night
+    come on dance with me
+    I'm levitating
+    You can fly away with me tonight
+    You can fly away with me tonight
+    Baby let me take you for a ride
+    Yeah yeah yeah yeah yeah
+    I'm levitating Woo
+    You can fly away with me tonight
+    You can fly away with me tonight
+    Baby let me take you for a ride
+    Yeah yeah yeah yeah yeah Woo
+    My love is like a rocket
+    watch it blast off
+    And I'm feeling so electric
+    dance my ass off
+    And even if I wanted to
+    I can't stop
+    Yeah yeah yeah yeah yeah
+    My love is like a rocket
+    watch it blast off
+    And I'm feeling so electric
+    dance my ass off
+    And even if I wanted to
+    I can't stop
+    Yeah yeah yeah yeah yeah
+    You want me I want you baby
+    My sugar-boo I'm levitating
+    The Milky Way we're renegading
+    I got you moonlight
+    you're my starlight
+    I need you all night All night
+    come on dance with me Let's go
+    I'm levitating Woo
+    You can fly away with me tonight
+    Tonight
+    You can fly away with me tonight
+    Baby let me take you for a ride
+    Yeah yeah yeah yeah yeah
+    Take you for a ride
+    I'm levitating Woo
+    You can fly away
+    with me tonight Tonight
+    You can fly away with me tonight
+    Baby let me take you for a ride
+    Yeah yeah yeah yeah yeah
+    Let me take you for a ride
+    I got you moonlight
+    you're my starlight
+    I need you all night
+    come on dance with me
+    I'm levitating`,
+  },
+  {
+    songId: '33359317',
+    title: 'Peaches (Feat. Daniel Caesar & Giveon)',
+    imgUrl:
+      'https://image.bugsm.co.kr/album/images/original/151758/15175845.jpg?version=undefined',
+    artist: 'Justin Bieber',
+    lyrics: `I got my peaches out in Georgia
+    Oh yeah shit
+    I get my weed from California
+    That's that shit
+    I took my chick up to the North yeah
+    Badass bitch
+    I get my light right
+    from the source yeah
+    Yeah that's it
+    And I see you Oh
+    the way I breathe you in In
+    It's the texture of your skin
+    I wanna wrap my arms around you baby
+    Never let you go oh
+    And I say oh
+    there's nothing like your touch
+    It's the way you lift me up yeah
+    And I'll be right here with you
+    'til the end
+    I got my peaches out in Georgia
+    Oh yeah shit
+    I get my weed from California
+    That's that shit
+    I took my chick up to the North yeah
+    Badass bitch
+    I get my light right
+    from the source yeah
+    Yeah that's it
+    You ain't sure yet but I'm for ya'
+    All I could want all I can wish for
+    Nights alone that we miss more
+    And days we save as souvenirs
+    There's no time
+    I wanna make more time
+    And give you my whole life
+    I left my girl I'm in Mallorca
+    Hate to leave her call it torture
+    Remember when I couldn't hold her
+    Left the baggage for a mover
+    I got my peaches out in Georgia
+    Oh yeah shit
+    I get my weed from California
+    That's that shit
+    I took my chick up to the North yeah
+    Badass bitch
+    I get my light right
+    from the source yeah
+    Yeah that's it
+    I get the feeling so I'm sure Sure
+    Hand in my hand because I'm yours
+    I can't I can't pretend
+    I can't ignore you're right for me
+    Don't think you wanna know
+    just where I've been oh
+    Don't be distracted
+    The one I need is right in my arms Oh
+    Your kisses taste the sweetest with mine
+    And I'll be right here with you
+    'til end of time
+    I got my peaches out in Georgia
+    Oh yeah shit
+    I get my weed from California
+    That's that shit
+    I took my chick up to the North yeah
+    Badass bitch
+    I get my light right
+    from the source yeah
+    Yeah that's it
+    I got my peaches out in Georgia
+    Oh yeah shit
+    I get my weed from California
+    That's that shit
+    I took my chick up to the North yeah
+    Badass bitch
+    I get my light right
+    from the source yeah
+    Yeah that's it
+    I got my peaches out in Georgia
+    Oh yeah shit
+    I get my weed from California
+    That's that shit
+    I took my chick up to the North yeah
+    Badass bitch
+    I get my light right
+    from the source yeah
+    Yeah that's it
+    I got my peaches out in Georgia
+    Oh yeah shit
+    I get my weed from California
+    That's that shit
+    I took my chick up to the North yeah
+    Badass bitch
+    I get my light right
+    from the source yeah
+    Yeah that's it`,
+  },
+  {
+    songId: '33559180',
+    title: 'Alcohol-Free',
+    imgUrl:
+      'https://image.bugsm.co.kr/album/images/original/204017/20401773.jpg?version=undefined',
+    artist: 'TWICE (트와이스)',
+    lyrics: `너와 있을 땐 내게
+    신기한 변화가 있는데
+    자꾸 미소 짓게 돼
+    아무 일도 없는데
+    
+    자꾸 마법에 걸려
+    밤을 새워도 안 졸려
+    다른 생각 지워져
+    심장 소리는 커져
+    사랑이 참 쉬워져
+    그래서 빠지고 빠져 점점 너에게
+    That's what you do to me
+    
+    나는 Alcohol free 근데 취해
+    마신 게 하나도 없는데
+    너와 있을 때마다 이래
+    날 보는 네 눈빛 때문에
+    
+    너는 눈으로 마시는 내 Champagne, 내 Wine
+    내 Tequila, margarita
+    Mojito with lime
+    Sweet mimosa, pina colada
+    
+    I'm drunk in you
+    I'm drunk in you
+    
+    너는 정말 특별해
+    전혀 독하지 않은데
+    낮에 별이 뜨게 해
+    한 모금 마셨는데
+    
+    자꾸 마법에 걸려
+    밤을 새워도 안 졸려
+    다른 생각 지워져
+    심장 소리는 커져
+    사랑이 참 쉬워져
+    그래서 빠지고 빠져 점점 너에게
+    That's what you do to me
+    
+    나는 Alcohol free 근데 취해
+    마신 게 하나도 없는데
+    너와 있을 때마다 이래
+    날 보는 네 눈빛 때문에
+    
+    Alcohol 도수는 완전 0.0%
+    근데 마실 때마다 자꾸 길을 잃어
+    자고 일어나도 깨지가 않아
+    근데 이 기분 싫지가 않아
+    
+    Easy to the mouth and tummy
+    Like a drink made of honey
+    이 술 이름은 도대체 뭐니
+    Makes the whole world bright and sunny
+    
+    나는 Alcohol free 근데 취해
+    마신 게 하나도 없는데
+    너와 있을 때마다 이래
+    날 보는 네 눈빛 때문에
+    
+    너는 눈으로 마시는 내 Champagne, 내 Wine
+    내 Tequila, margarita
+    Mojito with lime
+    Sweet mimosa, pina colada
+    
+    I'm drunk in you
+    I'm drunk in you`,
+  },
+  {
+    songId: '33013877',
+    title: '잠이 오질 않네요',
+    imgUrl:
+      'https://image.bugsm.co.kr/album/images/original/9991/999186.jpg?version=undefined',
+    artist: '장범준',
+    lyrics: `당신은 날 설레게 만들어
+    조용한 내 마음 자꾸만 춤추게 해
+    얼마나 얼마나 날 떨리게 하는지
+    당신이 이 밤을 항상 잠 못 들게 해
+    매일 같은 밤 너를 생각하면서
+    유치한 노랠 들으며
+    심장이 춤을 추면서
+    오오 난 너를 기다리면서
+    유치한 노랠 부르며
+    심장이 춤을 추면서 워
+    나를 떨리게 하나요
+    그대 왜 나를 설레게 하나요 자꾸만
+    오늘도 잠 못 이루는 이 밤
+    아름다운 그대
+    나를 아프게 하나요
+    웃는 그대 왜 자꾸 설레게 하나요
+    하염없이
+    오늘 밤 잠이 오질 않네요
+    보고 싶은 그대여
+    당신이 날 힘들게 만들어
+    갑자기 내 마음 자꾸만 멍들게 해
+    얼마나 얼마나 잠 못 들게 하는지
+    고요한 내 마음 항상 시끄럽게 해
+    매일 같은 밤 너를 생각하면서
+    유치한 노랠 들으며
+    심장이 춤을 추면서
+    워 난 너를 기다리면서
+    유치한 노랠 부르며
+    심장이 춤을 추면서 워
+    나를 떨리게 하나요
+    그대 왜 나를 설레게 하나요 자꾸만
+    오늘도 잠 못 이루는 이 밤
+    아름다운 그대
+    나를 아프게 하나요
+    웃는 그대 왜 자꾸 설레게 하나요
+    하염없이
+    오늘 밤 잠이 오질 않네요
+    보고 싶은 그대여
+    매일 밤 너를 생각하면서
+    유치한 노랠 들으며
+    심장이 춤을 추면서
+    워오 난 너를 기다리면서
+    유치한 노랠 부르며
+    심장이 춤을 추면서 워
+    나를 떨리게 하나요
+    그대 왜 나를 설레게 하나요 자꾸만
+    오늘도 잠 못 이루는 이 밤
+    아름다운 그대여
+    나를 아프게 하나요
+    웃는 그대 왜 자꾸 설레게 하나요
+    하염없이
+    오늘 밤 잠이 오질 않네요
+    보고 싶은 그대여`,
+  },
+];
+
+export default songs;
