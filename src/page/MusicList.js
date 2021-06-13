1b12d190858bd40f62abf7946f500927fdc68f80
unknown <wkdus7h@naver.com>
2021-06-13 15:30:25 +0900

    Update MusicList page

--- src/page/MusicList.js
+++ src/page/MusicList.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from '../components/Header';
+import Searchbar from '../components/Searchbar';
+import SongList from '../components/SongList';
 
-const MusicList = () => {
-  return <div>music list</div>;
+const MusicList = ({ match }) => {
+  return (
+    <div>
+      <Header />
+      <Searchbar />
+      <SongList />
+    </div>
+  );
 };
 
 export default MusicList;
