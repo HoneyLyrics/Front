b2e3ab3634c542ee9fffb0638fe1973d96ff281c
unknown <wkdus7h@naver.com>
2021-06-15 14:24:13 +0900

    Connect Home & MusicList

--- src/components/MoodList.js
+++ src/components/MoodList.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import moods from '../asset/moods';
 
 const MoodList = () => {
   return (
     <div className="mood-container">
-      {moods.map((mood, index) => (
-        <div
-          key={mood.id}
-          className="mood-item"
-          style={{ background: mood.color }}
-        >
-          <div className="mood-name">{mood.moodName}</div>
-          <div className="short-desc">short description</div>
-        </div>
+      {moods.map(mood => (
+        <Link key={mood.id} to={`/musiclist/${mood.id}`}>
+          <div className="mood-item" style={{ background: mood.color }}>
+            <div className="mood-name">{mood.moodName}</div>
+            <div className="short-desc">short description</div>
+          </div>
+        </Link>
       ))}
     </div>
   );
