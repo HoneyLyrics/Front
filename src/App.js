1b71bc8f1dbd7adab138e488dd3f3f8ffdbf45e0
unknown <wkdus7h@naver.com>
2021-06-15 17:02:36 +0900

    Resolve issue #7

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router';
 import Home from './page/Home';
 import MusicDetail from './page/MusicDetail';
 import MusicList from './page/MusicList';
+import ScrollToTop from './lib/ScrollToTop';
 import './style/app.css';
 
 const App = () => {
   return (
     <div className="App">
+      <ScrollToTop />
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/musiclist/:moodid" component={MusicList} />
