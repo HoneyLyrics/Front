53528b885df4af3b230d84d844f195236bec50be
unknown <wkdus7h@naver.com>
2021-06-16 15:19:21 +0900

    Add axios

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SongItem from './SongItem';
 import songes from '../asset/songs';
+import axios from '../../node_modules/axios/index';
 
 const SongList = ({ moodid }) => {
-  // 여기서 api 처리
-  const songs = songes[Number(moodid) - 1];
+  const [loading, setLoading] = useState(false);
+  const [songs, setSongs] = useState(null);
+
+  useEffect(() => {
+    const fn = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get('/musiclist/?moodid=1');
+        console.log(res.data);
+        setSongs(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fn();
+  }, []);
+
+  const songs2 = songes[Number(moodid) - 1];
 
   return (
     <div className="song-list">
-      {songs.map(song => (
+      {songs2.map(song => (
         <SongItem key={song.songId} song={song} />
       ))}
     </div>
