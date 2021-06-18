d26064d826e10bc5f064e2e2caf91779fbc013b2
unknown <wkdus7h@naver.com>
2021-06-18 15:48:29 +0900

    Change get api function to App.js

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,40 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import SongItem from './SongItem';
 // import songes from '../asset/songs';
-import axios from '../../node_modules/axios/index';
 
-const SongList = ({ moodid }) => {
-  const [loading, setLoading] = useState(false);
-  const [songs, setSongs] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fn = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get(`/musiclist/?moodid=${moodid}`);
-        setSongs(res.data);
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
-    fn();
-  }, [moodid]);
-
-  // const songs2 = songes[Number(moodid) - 1];
-
-  if (loading) {
-    return <div>불러오는 중...</div>;
-  }
-
-  if (!songs) {
-    return null;
-  }
-
-  if (error) {
-    return <div>에러 발생{error}</div>;
-  }
+const SongList = ({ songs }) => {
+  // const songs2 = songes[0];
 
   return (
     <div className="song-list">
