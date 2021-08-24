68c528f43685cc031d0694f98de796eaa005af92
Hyeanwoo <wkdus7h@naver.com>
2021-08-25 00:16:34 +0900

    Apply styled to SongList component & Add LoadingWrapper

--- src/page/MusicListPage.js
+++ src/page/MusicListPage.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import Footer from '../components/common/Footer';
 import Header from '../components/common/Header';
 import SongListContainer from '../container/mood-list/SongListContainer';
 
 const MusicList = ({ match }) => {
   return (
-    <div>
+    <>
       <Header />
       <SongListContainer moodId={match.params.moodid} />
-    </div>
+      <Footer />
+    </>
   );
 };
 
